f4cec95d9a0409837f3d0e27b9daa25532876ac2
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2022-11-04 20:15:56 +0530

    request_box

---
+++ app/src/main/java/com/example/fastaf/DialogueActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fastaf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DialogueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialogue)
+    }
+}
